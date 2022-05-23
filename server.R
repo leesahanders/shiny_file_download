dd1438adc0c132f8eef6e675aec355a59c6829c8
leesahanders <lisa.anders@rstudio.com>
2022-05-23 15:14:46 -0500

    Updating project with additional file size details

---
+++ server.R
@@ -0,0 +1,36 @@
+server <- function(input, output) {
+  
+  # Reactive value for selected dataset ----
+  # datasetInput <- reactive({
+  #   switch(input$dataset,
+  #          "rock" = rock,
+  #          "pressure" = pressure,
+  #          "cars" = cars)
+  # })
+  
+  #Check size of file with object.size()
+  datasetInput <- reactive({
+    switch(input$dataset,
+           "rock" = rock,
+           "pressure" = pressure,
+           "cars_1648bytes" = cars,
+           "cars_4848bytes" = rbind(cars,cars,cars,cars,cars),
+           "cars_8848bytes" = rbind(cars,cars,cars,cars,cars,cars,cars,cars,cars,cars))
+  })
+  
+  # Table of selected dataset ----
+  output$table <- renderTable({
+    datasetInput()
+  })
+  
+  # Downloadable csv of selected dataset ----
+  output$downloadData <- downloadHandler(
+    filename = function() {
+      paste(input$dataset, ".csv", sep = "")
+    },
+    content = function(file) {
+      write.csv(datasetInput(), file, row.names = FALSE)
+    }
+  )
+  
+}
---
+++ ui.R
@@ -0,0 +1,35 @@
+#https://shiny.rstudio.com/articles/download.html
+
+
+ui <- fluidPage(
+  
+  # App title ----
+  titlePanel("Downloading Data"),
+  
+  # Sidebar layout with input and output definitions ----
+  sidebarLayout(
+    
+    # Sidebar panel for inputs ----
+    sidebarPanel(
+      
+      # Input: Choose dataset ----
+      selectInput("dataset", "Choose a dataset:",
+                  choices = c("rock", "pressure", 
+                              "cars_1648bytes", "cars_4848bytes", "cars_8848bytes")),
+      
+
+      
+      # Button
+      downloadButton("downloadData", "Download")
+      
+    ),
+    
+    # Main panel for displaying outputs ----
+    mainPanel(
+      
+      tableOutput("table")
+      
+    )
+    
+  )
+)
