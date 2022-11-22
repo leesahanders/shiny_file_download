61105ee7200f752600f6595a767a803cc95477a8
Lisa Anders <lisa.anders@rstudio.com>
2022-11-22 20:09:56 +0000

    Syncing changes

--- server.R
+++ server.R
@@ -15,7 +15,8 @@ library(shiny)
 library(usethis)
 library(remotes)
 library(renv) #renv::upgrade(version = "0.16.0")
-library(talkingheadr) #renv::install("dgruenew/talkingheadr")
+library(conflicted) #remotes::install_github("r-lib/conflicted") 
+#library(talkingheadr) #renv::install("dgruenew/talkingheadr")
 
 # reference: https://community.rstudio.com/t/setting-github-pat-for-rsconnect/127444 and https://docs.posit.co/connect/admin/r/package-management/#from-private-git-repositories 
 # Private package from: https://github.com/dgruenew/talkingheadr, https://rstudio.slack.com/archives/C04280LRVQT/p1667430400292769?thread_ts=1667422300.132889&cid=C04280LRVQT 
