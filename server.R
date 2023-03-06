712bb6173fc04f28e7d6ecf031c4b4370299dbd1
leesahanders <lisa.anders@posit.co>
2023-03-06 22:38:37 +0000

    Removing private package section

--- server.R
+++ server.R
@@ -13,24 +13,6 @@ library(shiny)
 library(usethis)
 library(remotes)
 library(renv) #renv::upgrade(version = "0.16.0")
-#library(conflicted) #remotes::install_github("r-lib/conflicted") 
-#library(talkingheadr) #renv::install("dgruenew/talkingheadr")
-
-# reference: https://community.rstudio.com/t/setting-github-pat-for-rsconnect/127444 and https://docs.posit.co/connect/admin/r/package-management/#from-private-git-repositories 
-# Private package from: https://github.com/dgruenew/talkingheadr, https://rstudio.slack.com/archives/C04280LRVQT/p1667430400292769?thread_ts=1667422300.132889&cid=C04280LRVQT 
-# Create a GitHub PAT via R with usethis::create_github_token() 
-# Set it with gitcreds::gitcreds_set() and Sys.setenv(GITHUB_PAT = "") 
-# Store as env variable with usethis::edit_r_environ()
-# Follow: https://carpentries.github.io/sandpaper-docs/github-pat.html 
-# For package installation follow: https://rstudio.github.io/renv/reference/install.html?q=authentica#remotes-syntax 
-# Test our pat with: remotes::install_github("r-lib/conflicted") 
-# Install our private package: renv::install("dgruenew/talkingheadr")
-# For publishing to Connect: Try setting the option packrat.authenticated.downloads.use.renv to TRUE, or installing the httr package.
-
-#options(packrat.authenticated.downloads.use.renv = TRUE)
-#options(renv.auth.talkingheadr = list(GITHUB_PAT = ""))
-#renv:::renv_remotes_resolve("github::dgruenew/talkingheadr")
-#renv::install("dgruenew/talkingheadr")
 
 ## These are some challenging packages
 #library(stringi)
